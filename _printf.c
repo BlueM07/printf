874d591baf35740e65ca98698b9dbd48836470e7
BlueM07 <mariamernissi@gmail.com>
2023-10-16 12:29:30 -0700

    r

--- _printf.c
+++ _printf.c
@@ -1,10 +1,9 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ * _printf - main function print a formated string
+ * @format: string
+ * Return: count of charatcers
  */
 int _printf(const char *format, ...)
 {
