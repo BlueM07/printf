419ec9da7b9f347c07a6fd4b92cf26b0a8380e88
BlueM07 <mariamernissi@gmail.com>
2023-10-16 12:40:51 -0700

    r

--- _printf.c
+++ _printf.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
  * _printf - printf function
+ * You don’t have to handle the flag characters
+ * You don’t have to handle field width
+ * You don’t have to handle precision
+ * You don’t have to handle the length modifiers
  * @format: const char pointer
  * Return: b_len
  */
